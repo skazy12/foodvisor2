8d9f0a6d267a972f4659432c03220b63b355f343
Omar Rodriguez <omarwrt8@gmail.com>
2022-12-06 14:42:41 -0400

    controller

--- lib/dto/comidaController.dart
+++ lib/dto/comidaController.dart
@@ -6,4 +6,45 @@ class ComidaController extends GetxController {
   double proteinas;
   double carbohidratos;
   double grasas;
+
+  //getters and setters
+  String getNombre() {
+    return nombre;
+  }
+
+  double getCalorias() {
+    return calorias;
+  }
+
+  double getProteinas() {
+    return proteinas;
+  }
+
+  double getCarbohidratos() {
+    return carbohidratos;
+  }
+
+  double getGrasas() {
+    return grasas;
+  }
+
+  void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  void setCalorias(double calorias) {
+    this.calorias = calorias;
+  }
+
+  void setProteinas(double proteinas) {
+    this.proteinas = proteinas;
+  }
+
+  void setCarbohidratos(double carbohidratos) {
+    this.carbohidratos = carbohidratos;
+  }
+
+  void setGrasas(double grasas) {
+    this.grasas = grasas;
+  }
 }
